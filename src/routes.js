cc562f5753c8c0af0cd81e88e555e699a1ca0cb8
Haniel Schirmer <hanielschirmer@gmail.com>
2021-05-21 15:43:37 -0300

    Link-Front-Back / Parte do back

--- src/routes.js
+++ src/routes.js
@@ -12,18 +12,18 @@ const Auth = require("./middlewares/authentication");
 
 //Session
 routes.post("/login", SessionController.signIn);
-routes.delete("/logout", SessionController.signOut);
+routes.delete("/logout", Auth.authenticateToken, SessionController.signOut);
 
 //Usuario
-routes.get("/usuario/:usuario_id", UsuarioValidator.getByID ,/*Auth.authenticateToken ,*/UsuarioController.getByID);
+routes.get("/usuario/:usuario_id", UsuarioValidator.getByID ,Auth.authenticateToken ,UsuarioController.getByID);
 routes.post("/usuario", UsuarioValidator.create ,UsuarioController.create); //usuario nao precisa estar logado p/ criar conta
-routes.put("/usuario/:usuario_id", UsuarioValidator.updateByID ,/*Auth.authenticateToken ,*/UsuarioController.updateByID);
-routes.delete("/usuario/:usuario_id", UsuarioValidator.deleteByID ,/*Auth.authenticateToken ,*/UsuarioController.deleteByID);
+routes.put("/usuario/:usuario_id", UsuarioValidator.updateByID ,Auth.authenticateToken ,UsuarioController.updateByID);
+routes.delete("/usuario/:usuario_id", UsuarioValidator.deleteByID ,Auth.authenticateToken ,UsuarioController.deleteByID);
 
 //Endereco
-routes.get("/endereco/:endereco_id", EnderecoValidator.getByID ,/*Auth.authenticateToken ,*/EnderecoController.getByID);
-routes.post("/endereco/:usuario_id", EnderecoValidator.create ,/*Auth.authenticateToken ,*/EnderecoController.create);
-routes.put("/endereco/:endereco_id", EnderecoValidator.updateByID ,/*Auth.authenticateToken ,*/EnderecoController.updateByID);
-routes.delete("/endereco/:endereco_id", EnderecoValidator.deleteByID ,/*Auth.authenticateToken ,*/EnderecoController.deleteByID);
+routes.get("/endereco/:endereco_id", EnderecoValidator.getByID ,Auth.authenticateToken ,EnderecoController.getByID);
+routes.post("/endereco/:usuario_id", EnderecoValidator.create ,Auth.authenticateToken ,EnderecoController.create);
+routes.put("/endereco/:endereco_id", EnderecoValidator.updateByID ,Auth.authenticateToken ,EnderecoController.updateByID);
+routes.delete("/endereco/:endereco_id", EnderecoValidator.deleteByID ,Auth.authenticateToken ,EnderecoController.deleteByID);
 
 module.exports = routes
